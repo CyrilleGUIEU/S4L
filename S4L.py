36d8a37d7ced612a8a05479d009df1dec3b63ae2
Cyrille <cyrille.guieu@gmail.com>
2019-05-12 17:58:48 -0300

    modified:   S4L.py 	From global variables to class and methods design

--- S4L.py
+++ S4L.py
@@ -20,82 +20,84 @@ WIDTH=mss.mss().monitors[1]['width']
 HEIGHT=mss.mss().monitors[1]['height']
 
 app = tk.Tk()
-app.attributes('-topmost', True)
-app.attributes('-fullscreen', 1)
 
-
-isCapturing=False
-
-
-print("""S4L is now running ...
-1. Reduce the window
-2. Press '"""+KEYNAME+"'"+
-"""
-3. Select a rectangle in the frozen screen
-4. Paste the paper clip in your Latex code editor
-5. Go back to step 2 if necessary
-      """)
-          
-
-def hideWindow(event):
-    global isCapturing,Ax,Ay
-    app.state('withdrawn')
-    app.quit()
-    isCapturing=False
-    Ax,Bx=min(Ax,event.x),max(Ax,event.x)
-    Ay,By=min(Ay,event.y),max(Ay,event.y)
-    capture=image.crop((Ax,Ay,Bx,By))
-    n=0
-    while True:
-        try:
-            file_name=FILEPREFIX+"0"*(NBDIGITS-len(str(n)))+str(n)+FILEFORMAT
-            f=open(file_name)
-            f.close()
-            n=n+1
-        except FileNotFoundError:
-            break
-    capture.save(file_name)
-    capture.close()
-    commandLatex=PREFIXLATEX+PATHLATEX+file_name+SUFFIXLATEX    
-    pyperclip.copy(commandLatex)
-    os.remove(IMAGETEMP)
-    print("Screenshot saved in: "+file_name)
-    print("Clipboard contains :"+commandLatex)
-def createRectangle(event):
-    global Ax,Ay,nRectangle,nHLine,nVLine
-    Ax,Ay=event.x,event.y
-    nHLine=canvas.create_line(0,Ay,WIDTH,Ay,dash=(4, 4),fill="red")
-    nVLine=canvas.create_line(Ax,0,Ax,HEIGHT,dash=(4, 4),fill="red")
-    nRectangle=canvas.create_rectangle(Ax,Ay,Ax+1,Ay+1)
-
-def modifyRectangle(event):
-    global nRectangle,nHLine,nVLine
-    canvas.delete(nRectangle)
-    canvas.delete(nHLine)
-    canvas.delete(nVLine)
-    nHLine=canvas.create_line(0,event.y,WIDTH,event.y,dash=(4, 4),fill="red")
-    nVLine=canvas.create_line(event.x,0,event.x,HEIGHT,dash=(4, 4),fill="red")
-    nRectangle=canvas.create_rectangle(Ax,Ay,event.x,event.y)
-    
-app.bind_all('<ButtonRelease-1>', hideWindow)
-app.bind_all('<Button-1>', createRectangle)
-app.bind_all('<B1-Motion>', modifyRectangle)
-
-while True:
-    if keyboard.is_pressed(KEYNAME) and isCapturing==False:
-        isCapturing=True
-        capture=mss.mss()
-        capture.shot(output=IMAGETEMP)
-        capture.close()
-        image = Image.open(IMAGETEMP)
-        photo = ImageTk.PhotoImage(image)
-        canvas = tk.Canvas(width=WIDTH, height=HEIGHT, bg='black',cursor='crosshair')
-        canvas.pack(expand=tk.YES, fill=tk.BOTH)
-        canvas.create_image(00, 00, image=photo, anchor=tk.NW)
-        app.state('normal')
-        app.mainloop()
-        try:
-            canvas.destroy()
-        except NameError:
-            pass
         
+class GUI:
+    def __init__(self, app):
+        self.app = app
+        self.app.attributes('-topmost', True)
+        self.app.attributes('-fullscreen', 1)
+
+
+        self.isCapturing=False
+
+        print("""S4L is now running ...
+    1. Reduce the window
+    2. Press '"""+KEYNAME+"'"+
+    """
+    3. Select a rectangle in the frozen screen
+    4. Paste the paper clip in your Latex code editor
+    5. Go back to step 2 if necessary
+          """)
+        app.bind_all('<ButtonRelease-1>', self.hideWindow)
+        self.app.bind_all('<Button-1>', self.createRectangle)
+        self.app.bind_all('<B1-Motion>', self.modifyRectangle)
+
+        while True:
+            if keyboard.is_pressed(KEYNAME) and self.isCapturing==False:
+                self.isCapturing=True
+                capture=mss.mss()
+                capture.shot(output=IMAGETEMP)
+                capture.close()
+                self.image = Image.open(IMAGETEMP)
+                self.canvas = tk.Canvas(self.app,width=WIDTH, height=HEIGHT, bg='black',cursor='crosshair')
+                self.canvas.photo = ImageTk.PhotoImage(self.image)
+                self.canvas.create_image(00, 00, image=self.canvas.photo, anchor=tk.NW)
+                self.canvas.pack(expand=tk.YES, fill=tk.BOTH)
+                self.app.state('normal')
+                self.app.mainloop()
+                try:
+                    self.canvas.destroy()
+                except NameError:
+                    pass
+
+    def hideWindow(self,event):
+        self.app.state('withdrawn')
+        self.app.quit()
+        self.isCapturing=False
+        self.Ax,self.Bx=min(self.Ax,event.x),max(self.Ax,event.x)
+        self.Ay,self.By=min(self.Ay,event.y),max(self.Ay,event.y)
+        capture=self.image.crop((self.Ax,self.Ay,self.Bx,self.By))
+        n=0
+        while True:
+            try:
+                file_name=FILEPREFIX+"0"*(NBDIGITS-len(str(n)))+str(n)+FILEFORMAT
+                f=open(file_name)
+                f.close()
+                n=n+1
+            except FileNotFoundError:
+                break
+        capture.save(file_name)
+        capture.close()
+        commandLatex=PREFIXLATEX+PATHLATEX+file_name+SUFFIXLATEX    
+        pyperclip.copy(commandLatex)
+        os.remove(IMAGETEMP)
+        print("Screenshot saved in: "+file_name)
+        print("Clipboard contains :"+commandLatex)
+    def createRectangle(self,event):
+        self.Ax,self.Ay=event.x,event.y
+        self.nHLine=self.canvas.create_line(0,self.Ay,WIDTH,self.Ay,dash=(4, 4),fill="red")
+        self.nVLine=self.canvas.create_line(self.Ax,0,self.Ax,HEIGHT,dash=(4, 4),fill="red")
+        self.nRectangle=self.canvas.create_rectangle(self.Ax,self.Ay,self.Ax+1,self.Ay+1)
+
+    def modifyRectangle(self,event):
+        self.canvas.delete(self.nRectangle)
+        self.canvas.delete(self.nHLine)
+        self.canvas.delete(self.nVLine)
+        self.nHLine=self.canvas.create_line(0,event.y,WIDTH,event.y,dash=(4, 4),fill="red")
+        self.nVLine=self.canvas.create_line(event.x,0,event.x,HEIGHT,dash=(4, 4),fill="red")
+        self.nRectangle=self.canvas.create_rectangle(self.Ax,self.Ay,event.x,event.y)
+
+
+app = tk.Toplevel()
+this_app = GUI(app)
